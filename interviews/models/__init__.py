6db6dfc03fbb156819a4f9b6d0a712edaf162c33
Juliana <valeuli12@gmail.com>
2019-06-04 16:09:50 -0500

    Cambios en el __init__.py

--- interviews/models/__init__.py
+++ interviews/models/__init__.py
@@ -1,4 +1,8 @@
 from interviews.models.person import Person
+from interviews.models.Address import Address
+from interviews.models.Interview import Interview
 
 
 (Person,)
+(Address,)
+(Interview,)
