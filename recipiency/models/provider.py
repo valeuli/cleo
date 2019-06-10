951d56bb6853f292d382b84274302dcc51d4ccca
Alejandro <amujica@bellbank.com>
2019-06-10 15:52:40 -0400

    added personal data to a provider

--- recipiency/models/provider.py
+++ recipiency/models/provider.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+from interviews.models.person import Person
+
 
 class Provider(models.Model):
     """
     Model for Provider
     """
+    personal_data = models.OneToOneField(
+        Person,
+        on_delete=models.CASCADE
+    )
     email = models.CharField(
         unique=True,
         max_length=30
