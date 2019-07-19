ca3233dfac2039ade9a3673dd90cb0a3f2835bce
Juliana <valeuli12@gmail.com>
2019-07-18 21:24:25 -0500

    Creacion de los controladores

---
+++ activities/views/receipt_item.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def view(request):
+    return HttpResponse('Ver recibo')
+
+
+def list_persons(request):
+    return HttpResponse('Ver lista de recibos')
+
+
+def registration(request):
+    return HttpResponse('Registrar un nuevo recibo')
