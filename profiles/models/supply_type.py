c65f4ad496d3af78d52fac6dccbb0674d02d70ae
Juliana <valeuli12@gmail.com>
2019-07-31 23:01:14 -0500

    Cambios en las plantillas

--- profiles/models/supply_type.py
+++ profiles/models/supply_type.py
@@ -6,7 +6,8 @@ class SupplyType(models.Model):
     Model for SupplyType
     """
     name = models.CharField(
-        max_length=50
+        max_length=50,
+        null=True,
     )
 
     def __str__(self):
