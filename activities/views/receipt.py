543eb3270c2fc97ca0bb72c46a330627ec01e501
Juliana <valeuli12@gmail.com>
2019-09-22 18:49:46 -0500

    controlador de receipt

--- activities/views/receipt.py
+++ activities/views/receipt.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from activities.models.receipt_item import Receipt
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
@@ -28,4 +28,21 @@ def registration(request):
 
 @require_POST
 def register(request):
-    pass
+    receitp_form = ReceiptForm(request.POST)
+
+    if not receipt_form.is_valid():
+        messages.error(request, receipt_form.errors)
+        data = {
+            'receipt_form': receipt_form,
+            'provider_form': provider_form,
+        }
+        return render(request, 'activities/register_receipt.html', context=data)
+
+    receipt = Receipt.objects.create(
+        date = receitp_form.cleaned_data['date'],
+        observations = receitp_form.cleaned_data['observations'],
+        provider = receitp_form.cleaned_data['provider']
+    )
+
+    messages.success(request, 'Recibo agregado con éxito')
+    return redirect('list_receipt')
